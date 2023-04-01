e64685c22ebf5771dd7250fb683c7f80315181d7
slizhva <slizhva@gmail.com>
2023-04-01 12:26:30 +0300

    init commit

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+// Sets
+Route::get('/sets', [\App\Http\Controllers\SetsController::class, 'sets'])->name('sets');
+Route::post('/set/add', [\App\Http\Controllers\SetsController::class, 'add'])->name('set.add');
+Route::post('/set/{set_id}/delete', [\App\Http\Controllers\SetsController::class, 'delete'])->name('set.delete');
+
+// recipe
+Route::get('/recipe/{set_id}', [\App\Http\Controllers\RecipeController::class, 'recipe'])->name('recipe');
+Route::post('/recipe/{set_id}/add', [\App\Http\Controllers\RecipeController::class, 'add'])->name('recipe.add');
+Route::post('/recipe/{set_id}/delete', [\App\Http\Controllers\RecipeController::class, 'delete'])->name('recipe.delete');
+Route::post('/recipe/{set_id}/update', [\App\Http\Controllers\RecipeController::class, 'update'])->name('recipe.update');
+
